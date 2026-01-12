e13213a8069c384344668aee5e1e221ada355053
perialymba077 <perialymba077@gmail.com>
2026-01-12 12:56:16 +0600

    oops

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  "scripts": {
-  "dev": "vite",
-  "build": "vite build",
-  "preview": "vite preview"
-}
+  base: './', 
 })
